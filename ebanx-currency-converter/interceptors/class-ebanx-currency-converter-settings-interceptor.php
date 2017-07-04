54bd3b4dbc290ade3821375c18685e8efc4c24ca
Guilherme Pressutto <gpressutto5@gmail.com>
2017-07-03 23:13:29 -0300

    Added Interceptor

---
+++ ebanx-currency-converter/interceptors/class-ebanx-currency-converter-settings-interceptor.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Ebanx_Currency_converter_Settings_Interceptor
+{
+    /**
+     * Identify interceptor instances
+     *
+     * @var string
+     */
+    public $id = "settings";
+
+    /**
+     * Removes some form fields from settings to prevent use
+     *
+     * @param  array $fields The original form fields from EBANX plugin
+     * @return array The modified form fields
+     */
+    public function form_fields($fields)
+    {
+        /**
+         * Add a field to EBANX settings
+         * $fields['field'] = [
+         * 'title' => __('Title', 'ebanx-currency-converter'),
+         * 'label' => __('Label', 'ebanx-currency-converter'),
+         * 'type' => 'checkbox',
+         * ];
+         */
+
+        return $fields;
+    }
+}

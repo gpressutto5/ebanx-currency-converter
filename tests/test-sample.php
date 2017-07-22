51d1d86209e6537d3b8cf2de86877512f6a4b1fa
Guilherme Pressutto <gpressutto5@gmail.com>
2017-07-21 21:32:34 -0300

    Preparing test suite

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Ebanx_Currency_Converter_2
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

358d294b6f7569e36f4ddefd7568ec1f241895f8
Guilherme Pressutto <gpressutto5@gmail.com>
2017-09-11 19:29:36 -0300

    HTML and CSS done

--- ebanx-currency-converter/widgets/templates/public.template.php
+++ ebanx-currency-converter/widgets/templates/public.template.php
@@ -4,20 +4,37 @@
 } ?>
 
 <!-- WIDGET BODY-->
-<div class="ebanx-currency-converter--body">
-    <ul class="ebanx-currency-converter ebanx-currency-converter--flag-list">
+<div class="ebanx-currency-converter--clearfix">
+  <!-- Button -->
+  <button id="ebanx-currency-converter--dropdown-button" class="ebanx-currency-converter--reset-button">
+    <img class="ebanx-currency-converter--flag-image"
+         src="<?= EBANX_CURRENCY_CONVERTER_PLUGIN_DIR_URL ?>assets/images/br.svg"
+         alt="Brazil">
+    <span class="ebanx-currency-converter--country-text">BRL - Brazilian Real</span>
+    <i class="fa fa-caret-down ebanx-currency-converter--caret"></i>
+  </button>
+  <!-- /Button -->
+
+
+  <!-- Content -->
+  <div id="ebanx-currency-converter--select-currency" class="ebanx-currency-converter--select-currency ebanx-currency-converter--select-currency--active">
+    <ul class="ebanx-currency-converter--items">
         <?php foreach ($countries as $country):
             if ($country['enabled']): ?>
-                <li class="ebanx-currency-converter--flag-item">
-                    <button class="ebanx-currency-converter--flag-link ebanx-currency-converter--reset-button" data-country="<?= $country['country-code'] ?>" data-currency="<?= $country['currency-code'] ?>">
-                        <img class="ebanx-currency-converter--flag-image"
-                             src="<?= EBANX_CURRENCY_CONVERTER_PLUGIN_DIR_URL ?>assets/images/<?= $country['country-code'] ?>.svg"
-                             alt="<?= $country['name'] ?>">
-                    </button>
-                </li>
+              <li class="ebanx-currency-converter--flag-item">
+                <button class="ebanx-currency-converter--flag-link ebanx-currency-converter--reset-button"
+                        data-country="<?= $country['country-code'] ?>" data-currency="<?= $country['currency-code'] ?>">
+                  <img class="ebanx-currency-converter--flag-image"
+                       src="<?= EBANX_CURRENCY_CONVERTER_PLUGIN_DIR_URL ?>assets/images/<?= $country['country-code'] ?>.svg"
+                       alt="<?= $country['name'] ?>">
+                  <span class="ebanx-currency-converter--country-text">COP - Pesos Colombianos</span>
+                </button>
+              </li>
             <?php endif;
         endforeach; ?>
     </ul>
+  </div>
+  <!-- /Content -->
 </div>
 <!-- /WIDGET BODY-->
 
